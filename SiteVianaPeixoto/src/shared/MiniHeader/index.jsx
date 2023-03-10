b4becad5f776ed1f296d80c20f2ee97984137dab
gustavonunes0 <gustavonoroes@outlook.com>
2023-03-10 12:39:54 -0300

    aa2

--- SiteVianaPeixoto/src/shared/MiniHeader/index.jsx
+++ SiteVianaPeixoto/src/shared/MiniHeader/index.jsx
@@ -1,9 +1,9 @@
 import * as S from "./styles";
-import instagram from "../../../public/instagramicon.svg";
-import facebook from "../../assets/images/facebookicon.svg";
-import linkedin from "../../assets/images/linkedinicon.svg";
-import telefone from "../../assets/images/telefone.svg";
-import email from "../../assets/images/email.svg";
+// import instagram from "../../../public/fotos/instagramicon.svg";
+// import facebook from "../../assets/images/facebookicon.svg";
+// import linkedin from "../../assets/images/linkedinicon.svg";
+// import telefone from "../../assets/images/telefone.svg";
+// import email from "../../assets/images/email.svg";
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -11,25 +11,25 @@ const Header = () => {
     <S.MiniHeaderContainer>
       <S.MiniHeaderBox>
         <S.BoxSetor1>
-          <S.MiniMenu>
+          {/* <S.MiniMenu>
             <S.LogoIcons src={telefone} alt="Logo" />
             <S.MiniMenuItem><Link to="/" style={{ textDecoration: 'none', color: '#edeeed' }}>(85) 3033-6161</Link></S.MiniMenuItem>
           </S.MiniMenu>
           <S.MiniMenu>
             <S.LogoIcons src={email} alt="Logo" />
             <S.MiniMenuItem><Link to="/sobre" style={{ textDecoration: 'none', color: '#edeeed' }}>contato@vianapeicoto.com.br</Link></S.MiniMenuItem>
-          </S.MiniMenu>
+          </S.MiniMenu> */}
         </S.BoxSetor1>
         <S.BoxSetor2>
         <Link to="https://www.instagram.com/vianapeixotoadvogados/" target="_blank">
-          <S.LogoSocials src={instagram} alt="Logo" />
+          <S.LogoSocials src={"/fotos/instagramicon.svg"} alt="Logo" />
         </Link>
-        <Link to="https://www.facebook.com/VianaPeixotoAdv" target="_blank">
+        {/* <Link to="https://www.facebook.com/VianaPeixotoAdv" target="_blank">
           <S.LogoSocials src={facebook} alt="Logo" />
         </Link>
         <Link to="https://www.linkedin.com/company/viana-peixoto-advogados-associados/mycompany/" target="_blank">
           <S.LogoSocials src={linkedin} alt="Logo" />
-        </Link>
+        </Link> */}
         </S.BoxSetor2>
       </S.MiniHeaderBox>
     </S.MiniHeaderContainer>
